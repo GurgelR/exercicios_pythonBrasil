062b1329699da016beb8b10aa8a43890c7235d26
GurgelR <104876348+GurgelR@users.noreply.github.com>
2022-05-29 21:05:40 -0300

    Update 17.py

--- 1. EstruturaSequencial/17.py
+++ 1. EstruturaSequencial/17.py
@@ -9,16 +9,55 @@
 # 
 #       a) comprar apenas latas de 18 litros;
 #       b) comprar apenas galões de 3,6 litros;
-#       c)misturar latas e galões, de forma que o desperdício de tinta seja menor. 
+#       c) misturar latas e galões, de forma que o desperdício de tinta seja menor. 
 #       Acrescente 10% de folga e sempre arredonde os valores para cima, isto é, 
 #       considere latas cheias.
 # =============================================================================
 
-area_pintada = float (input ("Digite a área a ser pintada em m²: "))
+
+area_parede = float(input("Digite o tamanho da parede em m²: "))
+tinta_necessaria = area_parede/6
+print (f"Você precisará de {tinta_necessaria:,.2f} litros de tinta")
+
 preco_litro_galao = 25/3.6
 preco_litro_lata = 80/18
 
-print(preco_litro_galao)
-print(preco_litro_lata)
+###########   SITUAÇÃO 1   ###########
+
+latas = area_parede/(6*18)
+if float.is_integer(latas): # verifica se a variável de tipo float representa um número inteiro
+    latas = int (latas)
+    sobra_lata = 0
+else:
+    latas = int(latas)+1
+    sobra_lata = (latas*18) - (tinta_necessaria)
+
+preco_a = latas * 80
+
+print (f"\nCaso queira comprar SOMENTE LATAS DE 18 LITROS, você precisará de {latas}, o que custará R${preco_a:,.2f}. A sobra será de {sobra_lata:,.2f} litro(s)")
+
+###########   SITUAÇÃO 2   ###########
+
+galoes = area_parede/(6*3.6)
+if float.is_integer(galoes): # verifica se a variável de tipo float representa um número inteiro
+    galoes = int (galoes)
+    sobra_galao = 0
+else:
+    galoes = int(galoes)+1
+    sobra_galao = (galoes*3.6) - (tinta_necessaria)
+
+preco_b = galoes * 25
+
+print (f"\nCaso queira comprar SOMENTE GALÕES DE 3,6 LITROS, você precisará de {galoes}, o que custará R${preco_b:,.2f}. A sobra será de {sobra_galao:,.2f} litro(s)")
+
+###########   SITUAÇÃO 3   ###########
+
+area_folga = (area_parede + area_parede*0.1) # 10% de folga
+tinta_folga = area_folga/6 # tinta necessária
+latas_folga = int(tinta_folga/18) # latas que serão utilizadas
+galoes_folga = latas_folga*18 # 
+print (f"{tinta_folga:,.2f}", " - ", latas_folga)
 
 
+
+print (area_folga)
